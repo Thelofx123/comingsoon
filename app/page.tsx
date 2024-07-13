ca319c3f7d5fb4e737e8b111811e8760e05791c5
Gantulga <gaanbuud106@gmail.com>
2024-07-13 22:49:46 +0800

    done

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,17 @@ import LiveSvg from "@/components/mongolz"
 import Image from "next/image"
 export default function Home() {
 	return (
-		<main className="flex h-screen w-full flex-col items-center justify-center px-[42px] ">
+		<main className="flex h-screen w-full flex-col items-center justify-center px-[42px] bg-[#111111]">
+			<div className="absolute w-full h-screen z-10">
+				<Image
+					src="https://gobicashmere-res.cloudinary.com/image/upload/v1720881881/hee_unmtfp.png"
+					fill
+					className="object-cover w-full h-full"
+					alt="Hee"
+				/>
+			</div>
 			<div
-				className="flex items-center justify-center w-[291px] h-[282px] lg:w-[346px] lg:h-[468px] px-[49px]"
+				className="flex items-center justify-center w-[291px] h-[282px] lg:w-[346px] lg:h-[468px] px-[49px] z-20"
 				style={{ backgroundImage: "url('/path/to/image.jpg')" }}>
 				<LiveSvg className="" />
 			</div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		domains: [
+			"uploadthing.com",
+			"lh3.googleusercontent.com",
+			"utfs.io",
+			"s3-alpha-sig.figma.com",
+			"garnetod.files.wordpress.com",
+			"images.unsplash.com",
+			"media.graphassets.com",
+			"via.placeholder.com",
+			"cdn.sanity.io",
+			"gobicashmere-res.cloudinary.com",
+		],
+		formats: ["image/avif", "image/webp"],
+		minimumCacheTTL: 31536000,
+		deviceSizes: [375, 640, 750, 828, 1080, 1440, 1920, 2048, 3840],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 448],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
